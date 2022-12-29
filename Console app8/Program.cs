6a97cd81233bf141b4b0334fd499e8ea79c05bed
M7modgithub <116120756+M7modgithub@users.noreply.github.com>
2022-12-29 16:37:34 +0200

    Add files via upload

---
+++ Console app8/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+public class Exercise34
+{
+    public static void Main()
+    {
+        int num, i, M, S, E;
+
+        Console.WriteLine();
+
+
+        Console.WriteLine("Find the prime numbers within a range of numbers:");
+
+
+        Console.Write("*************************************************");
+
+
+        Console.WriteLine();
+
+
+        Console.Write("Input starting number of range: ");                S = Convert.ToInt32(Console.ReadLine());
+
+
+        Console.Write("Input ending number of range : ");                 E = Convert.ToInt32(Console.ReadLine());
+
+
+        Console.Write("The prime numbers between {0} and {1} are : \n", S, E);
+
+
+        for (num = S; num <= E; num++)
+        {
+            M = 0;
+
+            for (i = 2; i <= num / 2; i++)
+            {
+                if (num % i == 0)
+                {
+                    M++;
+                    break;
+                }
+            }
+
+            if (M == 0 && num != 1)
+                Console.Write("{0} ", num);
+        }
+        Console.Write("\n");
+    }
+}
---
+++ Console app9/Program.cs
@@ -0,0 +1,45 @@
+﻿
+using System;
+public class Exercise28
+{
+    public static void Main()
+    {
+        int n, i, sum;
+        int N, X;
+
+        Console.WriteLine();
+        Console.WriteLine("Find perfect numbers within a given number of range:");
+        Console.WriteLine("****************************************************");
+
+        Console.WriteLine();
+
+
+
+        Console.Write("Input the starting range of number : "); N = Convert.ToInt32(Console.ReadLine());
+
+
+
+        Console.Write("Input the ending range of number : "); X = Convert.ToInt32(Console.ReadLine());
+
+
+        Console.Write("The Perfect numbers within the given range : ");
+
+        for (n = N; n <= X; n++)
+        {
+            i = 1;
+            sum = 0;
+            while (i < n)
+            {
+                if (n % i == 0)
+                    sum = sum + i;
+                i++;
+            }
+            if (sum == n)
+                Console.Write("{0} ", n);
+        }
+        Console.WriteLine();
+        /*Perfect number is a positive number which sum of all positive divisors excluding*/
+        /*that number is equal to that number. For example 6 is perfect number since divisor of 6 are 1, 2 and 3.*/
+
+    }
+}
